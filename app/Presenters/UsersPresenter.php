3572471b46028f5906efe5a4980752f733ec3e74
petak23 <petak23@users.noreply.github.com>
2023-12-20 12:36:56 +0100

    Update UsersPresenter.php
    Update log in action.

--- app/Presenters/UsersPresenter.php
+++ app/Presenters/UsersPresenter.php
@@ -3,10 +3,11 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Utils\Validators;
 
 /**
  * Prezenter pre pristup k api užívateľov.
- * Posledna zmena(last change): 21.11.2023
+ * Posledna zmena(last change): 20.12.2023
  *
  * Modul: API
  *
@@ -14,8 +15,9 @@ use Nette;
  * @copyright  Copyright (c) 2012 - 2023 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.1
+ * @version 1.0.2
  * @help 1.) https://forum.nette.org/cs/28370-data-z-post-request-body-reactjs-appka-se-po-ceste-do-php-ztrati
+ * @help 2.) https://www.php.net/manual/en/function.checkdnsrr.php#48157
  */
 class UsersPresenter extends BasePresenter
 {
@@ -45,6 +47,13 @@ class UsersPresenter extends BasePresenter
 		$_post = json_decode(file_get_contents("php://input"), true); // @help 1.)
 
 		try {
+			if (!Validators::isEmail($post['email'])) { // Kontrola, či bol zadaný email v správnom tvare
+				throw new Nette\InvalidArgumentException;
+			}
+			if(!checkdnsrr(array_pop(explode("@",$email)),"MX")){ // @help 2.)
+        throw new Nette\InvalidArgumentException;						// Kontrola, či daná doména existuje
+			}
+			
 			$this->user->login($post['email'], $post['password']);
 			$this->sendJson(
 				$this->user_main->getUser(
@@ -55,7 +64,9 @@ class UsersPresenter extends BasePresenter
 				)
 			);
 		} catch (Nette\Security\AuthenticationException $e) {
-			$this->sendJson(['error'=>'Uživateľské meno alebo heslo je nesprávne!!!']);
+			$this->sendJson(['status'=>500, 'error'=>'Uživateľské meno alebo heslo je nesprávne!!!']);
+		} catch (Nette\InvalidArgumentException $e) {
+			$this->sendJson(['status'=>500, 'error'=>'Zadajte email v správnom tvare!!!']);
 		}
 		
 	}
