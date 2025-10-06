1c910931f555a1c693495a8251a930f5b88330f2
petak23 <petak23@gmail.com>
2025-10-06 13:37:22 +0200

    2025-10-06_02 - Pokus

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -62,15 +62,26 @@ abstract class BasePresenter extends Presenter
 		$httpRequest = $this->getHttpRequest();
 		$httpResponse = $this->getHttpResponse();
 
-		// CORS hlavičky (rovnaké ako v index.php)
-		$httpResponse->setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+		// Rozlíšenie prostredia podľa hostname
+		$isLocalhost = in_array($httpRequest->getUrl()->getHost(), ['localhost', '127.0.0.1'], true);
+
+		$allowedOrigins = $isLocalhost
+				? ['http://localhost:5173']
+				: ['https://echo-msz.eu'];
+
+		$origin = $httpRequest->getHeader('Origin');
+		if ($origin && in_array($origin, $allowedOrigins, true)) {
+				$httpResponse->setHeader('Access-Control-Allow-Origin', $origin);
+		} else {
+				$httpResponse->setHeader('Access-Control-Allow-Origin', $allowedOrigins[0]);
+		}
 		$httpResponse->setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 		$httpResponse->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 		$httpResponse->setHeader('Access-Control-Allow-Credentials', 'true');
 
 		if ($httpRequest->getMethod() === 'OPTIONS') {
-			$httpResponse->setCode(IResponse::S204_NoContent); // 204 = no content
-			$this->terminate(); // okamžité ukončenie requestu
+				$httpResponse->setCode(IResponse::S204_NoContent); // 204 = no content
+				$this->terminate(); // okamžité ukončenie requestu
 		}
 
 		// Sprava uzivatela
