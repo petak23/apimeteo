38009f0442fdbf8e089e8afea9f1e3c502197135
petak23 <petak23@gmail.com>
2025-10-19 09:04:01 +0200

    2025-10-19_01 - Opravy

--- sql/change.sql
+++ sql/change.sql
@@ -1,1 +1,5 @@
 UPDATE `user_permission` SET `actions` = 'users,user,default,save' WHERE `id` = '29';
+INSERT INTO `user_resource` (`name`)
+VALUES ('Json');
+INSERT INTO `user_permission` (`id_user_roles`, `id_user_resource`, `actions`)
+VALUES ('1', '12', NULL);
